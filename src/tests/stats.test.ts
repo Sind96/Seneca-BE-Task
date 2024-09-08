65f1543d8564c8b21d9437b8b79fa9940165e377
Sind96 <sindhujany@gmail.com>
2024-09-08 13:35:45 +0100

    test: verify first test for fetchCourseLifetimeStats is working correctly

--- src/tests/stats.test.ts
+++ src/tests/stats.test.ts
@@ -1,7 +1,11 @@
 import request from "supertest";
 import express from "express";
 import mongoose from "mongoose";
-import { persistStudySession } from "../controllers/stats";
+import {
+  fetchCourseLifetimeStats,
+  persistStudySession,
+} from "../controllers/stats";
+import { Stats } from "../models/stats";
 
 const app = express();
 app.use(express.json());
@@ -37,5 +41,54 @@ describe("POST to /courses/:courseId", () => {
     expect(response.body).toHaveProperty("totalModulesStudied", 5);
     expect(response.body).toHaveProperty("averageScore", 75);
     expect(response.body).toHaveProperty("timeStudied", 600000);
+  });
+});
+
+// Test for fetchCourseLifetimeStats controller function
+// This test checks that a GET request to /course/:courseId with valid headers returns
+// aggregated statistics for the specified course and returns a 200 status with the expected response body.
+
+app.get("/courses/:courseId", fetchCourseLifetimeStats);
+
+describe("GET to /courses/:courseId", () => {
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://localhost:27017/testdb");
+
+    //Adding temporary data to database
+    await Stats.create([
+      {
+        xuserid: "Sindhu",
+        courseId: "maths",
+        sessionId: "test1",
+        totalModulesStudied: 5,
+        averageScore: 80,
+        timeStudied: 50000,
+      },
+      {
+        xuserid: "Sindhu",
+        courseId: "maths",
+        sessionId: "test2",
+        totalModulesStudied: 2,
+        averageScore: 60,
+        timeStudied: 30000,
+      },
+    ]);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+  });
+
+  it("should fetch course lifetime stats and return 200", async () => {
+    const response = await request(app)
+      .get("/courses/maths")
+      .set("xuserid", "Sindhu");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(1);
+    expect(response.body[0]).toHaveProperty("totalModulesStudied", 7);
+    expect(response.body[0]).toHaveProperty("averageScore", 70);
+    expect(response.body[0]).toHaveProperty("timeStudied", 80000);
   });
 });
